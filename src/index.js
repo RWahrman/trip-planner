aa0b19954f4a917a61e7878c6c1d37fc7bb28df3
RWahrman <rebecca.wahrman@gmail.com>
2020-06-30 11:50:23 -0400

    tried to create marker types

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicndhaHJtYW4iLCJhIjoiY2s3dXZqd2ttMTV1dTNlbnF6bW83MjIwOSJ9.GISqLzB3By9cnceIUy6QaA";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705],
+  zoom: 12,
+  style: "mapbox://styles/mapbox/streets-v10", // mapbox has lots of different map styles available.
+});
+
+const markerDomEl = document.createElement("div");
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
---
+++ src/marker.js
@@ -0,0 +1,24 @@
+const mapbox = require("mapbox-gl");
+
+const iconURLs = {
+  hotels: "http://i.imgur.com/D9574Cu.png",
+  restaurants: "http://i.imgur.com/cqR6pUI.png",
+  activities: "http://i.imgur.com/WbMOfMl.png",
+};
+
+const buildMarker = function (type, coords) {
+  // Your Code Here
+  const markerDomEl = document.createElement("div");
+  markerDomEl.style.width = "32px";
+  markerDomEl.style.height = "39px";
+  markerDomEl.style.backgroundImage = `url(${iconURLs[type]})`;
+  new mapboxgl.Marker(markerDomEl).setLngLat(coords).addTo(map);
+};
+
+module.exports = buildMarker;
+
+const markerDomEl = document.createElement("div");
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
